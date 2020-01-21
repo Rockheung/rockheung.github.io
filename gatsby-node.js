273fd8e2a86acabcb1fbc1577765489528a78c22
Rockheung <rockheung@gmail.com>
2020-01-22 08:37:56 +0900

    Update: post template

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,4 @@
+const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
@@ -13,8 +14,7 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
 }
 
 exports.createPages = async ({ graphql, actions }) => {
-  // **Note:** The graphql function call returns a Promise
-  // see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise for more info
+  const { createPage } = actions
   const result = await graphql(`
     query {
       allMarkdownRemark {
@@ -28,6 +28,7 @@ exports.createPages = async ({ graphql, actions }) => {
       }
     }
   `)
+
   result.data.allMarkdownRemark.edges.forEach(({ node }) => {
     createPage({
       path: node.fields.slug,
