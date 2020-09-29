f57c745e65b8dcf9d7f6930d3321b300e969fcc8
박흥준 Rock <rockheung@gmail.com>
2020-09-29 20:49:20 +0900

    Re-init

--- gatsby-browser.js
+++
@@ -1,7 +0,0 @@
-import './src/styles/global.css';
-import React from 'react';
-import { ThemeProvider } from './src/context/Neumorphism';
-
-export const wrapRootElement = ({ element }) => (
-  <ThemeProvider>{element}</ThemeProvider>
-);
